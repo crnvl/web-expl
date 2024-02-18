bcc833e4490c21bdde44241ebccc577d84dfcecd
Luna <lsaak@byte5.de>
2024-02-18 23:42:21 +0100

    feat(seo): add metadata

--- src/layout/page-layout.tsx
+++ src/layout/page-layout.tsx
@@ -13,7 +13,20 @@ const PageLayout: FC<IProps> = (props) => {
     <>
       <Head>
         <title>{props.title}</title>
+        <meta name="title" content={props.title} />
         <meta name="description" content={props.description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://web-expl.qwq.sh" />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={props.description} />
+        <meta property="og:image" content="/banner.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://web-expl.qwq.sh" />
+        <meta property="twitter:title" content={props.title} />
+        <meta property="twitter:description" content={props.description} />
+        <meta property="twitter:image" content="/banner.png" />
       </Head>
       <div className="bg-background text-text overflow-x-hidden">
         <Navbar title={props.title} />

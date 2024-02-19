c8e864541764a1f6bbd81b0c8f05c59160a4da12
Luna <lsaak@byte5.de>
2024-02-19 18:23:33 +0100

    feat(meta): remove og url

--- src/layout/page-layout.tsx
+++ src/layout/page-layout.tsx
@@ -17,13 +17,11 @@ const PageLayout: FC<IProps> = (props) => {
         <meta name="description" content={props.description} />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://web-expl.qwq.sh" />
         <meta property="og:title" content={props.title} />
         <meta property="og:description" content={props.description} />
         <meta property="og:image" content="/banner.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://web-expl.qwq.sh" />
         <meta property="twitter:title" content={props.title} />
         <meta property="twitter:description" content={props.description} />
         <meta property="twitter:image" content="/banner.png" />
